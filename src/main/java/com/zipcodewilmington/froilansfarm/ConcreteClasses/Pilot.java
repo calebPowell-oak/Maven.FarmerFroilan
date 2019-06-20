8e65c7af695cf0e164363f8f8628f35c9caf9186
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-20 15:01:28 -0400

    Pilot now extends Person

--- src/main/java/com/zipcodewilmington/froilansfarm/ConcreteClasses/Pilot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ConcreteClasses/Pilot.java
@@ -1,10 +1,11 @@
 package com.zipcodewilmington.froilansfarm.ConcreteClasses;
 
+import com.zipcodewilmington.froilansfarm.AbstractClasses.Person;
 import com.zipcodewilmington.froilansfarm.Interfaces.Flyable;
 import com.zipcodewilmington.froilansfarm.Interfaces.Flyer;
 import com.zipcodewilmington.froilansfarm.Interfaces.Rideable;
 
-public class Pilot implements Flyer {
+public class Pilot extends Person implements Flyer {
 
     public void fly(Flyable flyable) {
 
