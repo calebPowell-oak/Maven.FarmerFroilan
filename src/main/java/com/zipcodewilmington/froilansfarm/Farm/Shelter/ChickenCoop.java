8384b8a3ccfdc2e19e7d471cf9ec4a0972d1347f
William Reyes <williamreyes814@yahoo.com>
2019-06-22 18:45:14 -0400

    Finished Test

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Shelter/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Shelter/ChickenCoop.java
@@ -3,4 +3,7 @@ package com.zipcodewilmington.froilansfarm.Farm.Shelter;
 import com.zipcodewilmington.froilansfarm.Farm.Produce.Chicken;
 
 public class ChickenCoop extends Shelter<Chicken> {
+
+
+
 }
