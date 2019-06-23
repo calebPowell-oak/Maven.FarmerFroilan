04a42d28db36dfd11fd63772300447583f2fd466
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-23 15:49:26 -0400

    Wrote tractor tests

--- src/test/java/com/zipcodewilmington/froilansfarm/Farm/Transportation/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Farm/Transportation/TractorTest.java
@@ -1,6 +1,14 @@
 package com.zipcodewilmington.froilansfarm.Farm.Transportation;
 
+import com.zipcodewilmington.froilansfarm.Farm.CropRow;
+import com.zipcodewilmington.froilansfarm.Farm.Farm;
+import com.zipcodewilmington.froilansfarm.Farm.Produce.TomatoPlant;
+import com.zipcodewilmington.froilansfarm.Human.Driver;
+import com.zipcodewilmington.froilansfarm.Human.Farmer;
+import org.junit.Assert;
 import org.junit.Test;
+
+import javax.xml.transform.TransformerConfigurationException;
 
 import static org.junit.Assert.*;
 
@@ -8,17 +16,78 @@ public class TractorTest {
 
     @Test
     public void operate() {
+        Farm farm = new Farm();
+        Tractor cropDuster = new Tractor();
+
+        cropDuster.operate(farm);
+        Farm actual = cropDuster.getCurrentFarm();
+
+        Assert.assertEquals(farm, actual);
     }
 
     @Test
     public void makeNoise() {
+        Tractor cropDuster = new Tractor();
+        String expected = "*tractor noises*";
+
+        String actual = cropDuster.makeNoise();
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void harvestCrops() {
+    public void harvest() {
+        CropRow<TomatoPlant> cropRow = new CropRow<>();
+        Tractor cropDuster = new Tractor();
+
+        cropDuster.harvestCrops(cropRow);
+
+        Assert.assertNotNull(null);// Harvest things!
     }
 
     @Test
-    public void takeDriver() {
+    public void takeDriver_trueWhenEmpty() {
+        Tractor cropDuster = new Tractor();
+        Farmer pilot = new Farmer("Yooo");
+
+        Boolean result = cropDuster.takeDriver(pilot);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void takePilot_falseWhenOccupied() {
+        Tractor cropDuster = new Tractor();
+        Farmer pilot = new Farmer("Yooo");
+        Farmer pilot2 = new Farmer("Zooo");
+
+        cropDuster.takeDriver(pilot);
+        Boolean result = cropDuster.takeDriver(pilot2);
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void takePilot_onePilotTries() {
+        Tractor cropDuster = new Tractor();
+        Farmer pilot = new Farmer("Yooo");
+
+        cropDuster.takeDriver(pilot);
+        Driver actual = cropDuster.getCurrentDriver();
+
+        Assert.assertEquals(pilot, actual);
+    }
+
+    @Test
+    public void takePilot_twoPilotsTry() {
+        Tractor cropDuster = new Tractor();
+        Farmer pilot = new Farmer("Yooo");
+        Farmer pilot2 = new Farmer("Zooo");
+
+        cropDuster.takeDriver(pilot);
+        cropDuster.takeDriver(pilot2);
+        Driver actual = cropDuster.getCurrentDriver();
+
+        Assert.assertEquals(pilot, actual);
     }
 }
