5944adbdc6120b8433b38297419d01817990013d
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-23 14:18:30 -0400

    Updates to transportation package to allow humans to use transport

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Transportation/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Transportation/CropDuster.java
@@ -2,17 +2,31 @@ package com.zipcodewilmington.froilansfarm.Farm.Transportation;
 
 import com.zipcodewilmington.froilansfarm.Farm.CropRow;
 import com.zipcodewilmington.froilansfarm.Farm.Farm;
+import com.zipcodewilmington.froilansfarm.Human.Pilot;
 
 public class CropDuster implements FarmVehicle, Flyable {
-    public void operate(Farm farm) {
+    private Pilot currentPilot;
+    private Farm currentFarm;
 
+    public void operate(Farm farm) {
+        currentFarm = farm;
     }
 
     public String makeNoise() {
-        return null;
+        return "Crop duster is making lots of Noise!";
     }
 
     public void fertilize(CropRow cropRow){
 
+    }
+
+    @Override
+    public Boolean takePilot(Pilot pilot) {
+        if(currentPilot == null){
+            currentPilot = pilot;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Transportation/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Transportation/Tractor.java
@@ -1,13 +1,27 @@
 package com.zipcodewilmington.froilansfarm.Farm.Transportation;
 
 import com.zipcodewilmington.froilansfarm.Farm.Farm;
+import com.zipcodewilmington.froilansfarm.Human.Driver;
 
 public class Tractor implements FarmVehicle, Driveable {
-    public void operate(Farm farm) {
+    private Driver currentDriver;
+    private Farm currentFarm;
 
+    public void operate(Farm farm) {
+        currentFarm = farm;
     }
 
     public String makeNoise() {
         return null;
+    }
+
+    @Override
+    public Boolean takeDriver(Driver driver) {
+        if(currentDriver == null){
+            currentDriver = driver;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
