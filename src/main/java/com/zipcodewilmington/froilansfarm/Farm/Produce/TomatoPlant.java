b8f1626a87a31aee64581334f502eca31b10a35a
sputnika <sputnika91@gmail.com>
2019-06-23 12:56:49 -0400

    chicken,tomatoplant

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Produce/TomatoPlant.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Produce/TomatoPlant.java
@@ -3,8 +3,17 @@ package com.zipcodewilmington.froilansfarm.Farm.Produce;
 import com.zipcodewilmington.froilansfarm.Farm.Produce.Edible.Tomato;
 
 public class TomatoPlant extends Crop {
+    private Boolean isFertilized;
+    private Boolean hasBeenHarvested;
+
+    public TomatoPlant () {
+        this.isFertilized=false;
+        this.hasBeenHarvested=false;
+    }
 
     public Tomato yield() {
-        return new Tomato();
+        if((isFertilized )&& (!hasBeenHarvested))
+            return new Tomato();
+        else return null;
     }
 }
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Farm/Produce/CropTest.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington.froilansfarm.Farm.Produce;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CropTest {
+
+    @Test
+    public void testImplementation(){
+
+
+
+
+    }
+
+}
