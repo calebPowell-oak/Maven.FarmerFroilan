d7f9a6a0257d0be474f77c2ed58655ce856dc6cd
Joanna Hsiao <chsiao58@gmail.com>
2019-06-23 21:55:49 -0400

    refactor Collection to List

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Farm.java
@@ -4,12 +4,14 @@ import com.zipcodewilmington.froilansfarm.Farm.Shelter.ChickenCoop;
 import com.zipcodewilmington.froilansfarm.Farm.Shelter.Farmhouse;
 import com.zipcodewilmington.froilansfarm.Farm.Shelter.Stable;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Farm {
-    Farmhouse fh;
-    Collection<Stable> stables;
-    Collection<ChickenCoop> chickenCoops;
+    Farmhouse fh = new Farmhouse();
+    List<Stable> stables = new ArrayList<>();
+    List<ChickenCoop> chickenCoops = new ArrayList<>();
     RefrigeratedFoodStorage foodStorage = new RefrigeratedFoodStorage();
     Field field = new Field();
 
@@ -19,5 +21,17 @@ public class Farm {
 
     public Field getField() {
         return field;
+    }
+
+    public Farmhouse getFarmhouse() {
+        return fh;
+    }
+
+    public List<Stable> getStables() {
+        return stables;
+    }
+
+    public List<ChickenCoop> getChickenCoops() {
+        return chickenCoops;
     }
 }
